d0e1eaf53b2c15a905a5ec8345e8f84c1d4db770
marcushammarberg <marcusoft.net@gmail.com>
2010-12-14 12:22:36 +0100

    Initial commit

---
+++ DemoSpecFlowTags/TagDemo.feature
@@ -0,0 +1,29 @@
+﻿Feature: Tag demonstrator
+	In order to show the capabilities of tags in SpecFlow
+	As a developer
+	I want to write scenarios that has tags and show their usage in code
+
+
+@ignore
+Scenario: Ignored scenario
+	Given that my scenario has the @ignore tag
+	When I run the scenario
+	Then the scenario is ignored
+		And the missing step definitions are not reported 
+
+Scenario: A scenario without tag
+	Given that my scenario has no tags
+	When I run the scenario
+	Then before scenario hook without tag is run
+	
+@testTag1
+Scenario: A scenario with 1 tag
+	Given that my scenario has 1 tag
+	When I run the scenario
+	Then before scenario hook with the tag is run
+
+@testTag1 @testTag2 @testTag3
+Scenario: A scenario with 3 tags
+	Given that my scenario has 3 tags
+	When I run the scenario
+	Then before scenario hook with the tags is run
---
+++ DemoSpecFlowTags/TagemonstrationSteps.cs
@@ -0,0 +1,107 @@
+﻿using System.Linq;
+using Should.Fluent;
+using TechTalk.SpecFlow;
+
+namespace DemoSpecFlowTags
+{
+    [Binding]
+    public class TagemonstrationSteps
+    {
+        private bool _beforeScenarioWithNoTags = false;
+        private bool _testTag1 = false;
+        private bool _testTag2 = false;
+        private bool _testTag3 = false;
+        private bool _testTags = false;
+
+        [BeforeScenario]
+        public void BeforeScenarioWith_0Tags()
+        {
+            _beforeScenarioWithNoTags = true;
+        }
+
+        [BeforeScenario("testTag1")]
+        public void BeforeScenario_testTag1()
+        {
+            _testTag1 = true;
+        }
+
+        [BeforeScenario("testTag2")] 
+        public void BeforeScenario_testTag2()
+        {
+            _testTag2 = true;
+        }
+
+        [BeforeScenario("testTag3")]
+        public void BeforeScenario_testTag3()
+        {
+            _testTag3 = true;
+        }
+
+        [BeforeScenario("testTag1", "testTag2", "testTag3")]
+        public void BeforeScenario_testTags()
+        {
+            _testTags = true;
+        }
+
+        [When(@"I run the scenario")]
+        public void WhenIRunTheScenario() 
+        {
+            // Nothing to do here
+        }
+
+        [Given(@"that my scenario has no tags")]
+        public void GivenThatMyScenarioHasNoTags()
+        {
+            ScenarioContext.Current.ScenarioInfo.Tags.Should().Be.Null();
+        }
+
+        [Given(@"that my scenario has 1 tag")]
+        public void GivenThatMyScenarioHas1Tag()
+        {
+            ScenarioContext.Current.ScenarioInfo.Tags.Should().Not.Be.Null();
+            ScenarioContext.Current.ScenarioInfo.Tags.ToList().Count.Should().Equal(1);
+        }
+
+        [Given(@"that my scenario has 3 tags")]
+        public void GivenThatMyScenarioHasMultipleTags()
+        {
+            ScenarioContext.Current.ScenarioInfo.Tags.Should().Not.Be.Null();
+            ScenarioContext.Current.ScenarioInfo.Tags.ToList().Count.Should().Equal(3);
+        }
+
+        [Then(@"before scenario hook without tag is run")]
+        public void ThenBeforeScenarioHookWithoutTagIsRun()
+        {
+            _beforeScenarioWithNoTags.Should().Be.True();
+
+            _testTag1.Should().Be.False();
+            _testTag2.Should().Be.False();
+            _testTag3.Should().Be.False();
+            _testTags.Should().Be.False();
+        }
+
+        [Then(@"before scenario hook with the tags is run")]
+        public void ThenBeforeScenarioHookWithTheTagsIsRun()
+        {
+            _beforeScenarioWithNoTags.Should().Be.True();
+            _testTag1.Should().Be.True();
+            _testTag2.Should().Be.True();
+            _testTag3.Should().Be.True();
+            _testTags.Should().Be.True();
+        }
+
+        [Then(@"before scenario hook with the tag is run")]
+        public void ThenBeforeScenarioHookWithTheTagIsRun()
+        {
+            _beforeScenarioWithNoTags.Should().Be.True();
+            _testTag1.Should().Be.True();
+            _testTags.Should().Be.True();
+
+
+            _testTag2.Should().Be.False();
+            _testTag3.Should().Be.False();
+        }
+
+        
+    }
+}
